bd35caa9e20c43a21fb3ffb109da756edb77604b
jquiceno <soyjquiceno@gmail.com>
2025-03-18 18:45:20 -0500

    Status property removed from Order entity
    - Update Order use case removed

--- src/modules/orders/__tests__/__mocks__/order.mock.ts
+++ src/modules/orders/__tests__/__mocks__/order.mock.ts
@@ -1,12 +1,10 @@
 import { Order } from '@modules/orders/domain/entities';
-import { OrderStatus } from '@modules/orders/domain/enums';
 
 interface CreateOrderMockProps {
   merchantId?: string;
   amount?: number;
   id?: string;
   createdAt?: Date;
-  status?: OrderStatus;
 }
 
 export const createOrderMock = ({
@@ -14,22 +12,17 @@ export const createOrderMock = ({
   amount = 1000,
   id,
   createdAt = new Date(),
-  status = OrderStatus.PENDING,
 }: CreateOrderMockProps = {}): Order => {
   const order = new Order({
     merchantId,
     amount,
   });
 
-  // Override readonly properties using Object.defineProperty
   if (id) {
     Object.defineProperty(order, 'id', { value: id });
   }
   if (createdAt) {
     Object.defineProperty(order, 'createdAt', { value: createdAt });
-  }
-  if (status) {
-    Object.defineProperty(order, 'status', { value: status });
   }
 
   return order;
--- src/modules/orders/application/use-cases/index.ts
+++ src/modules/orders/application/use-cases/index.ts
@@ -1,4 +1,3 @@
 export * from './create-order.use-case';
 export * from './get-orders-by-date-range.use-case';
-export * from './update-order-status.use-case';
 export * from './get-orders-by-merchant.use-case';
